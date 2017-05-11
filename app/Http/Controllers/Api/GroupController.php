c97c848a5e7d7d53027d3e4a570aaa9a5452c5b8
tic40 <ccpzjoh@gmail.com>
2017-05-11 21:55:49 +0900

    move transaction #7

--- app/Http/Controllers/Api/GroupController.php
+++ app/Http/Controllers/Api/GroupController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\GenerateGroup;
+use App\Services\Group;
 use App\Infrastructure\GroupRepository;
 use App\Infrastructure\EmployeeRepository;
 use App\Infrastructure\GroupMemberRepository;
@@ -44,23 +45,8 @@ class GroupController extends Controller
      */
     public function store(Request $request, int $year, int $month)
     {
-        \DB::beginTransaction();
-        try {
-            if (count(GroupRepository::getGroupsByTargetDate($year, $month)) > 0) {
-                throw new \Exception('Groups of this month already exists.');
-            }
-            $resultGroups = GroupRepository::storeGroups($year, $month, $request['groupList']);
-            $insertedGroups = GroupRepository::getGroupsByTargetDate($year, $month);
-            $resultGroupMembers = GroupMemberRepository::storeGroupMembers($request['groupList'], $insertedGroups);
-            \DB::commit();
-        } catch (\Exception $e) {
-            \DB::rollback();
-            return response()->json(
-                $e->getMessage()
-            );
-        }
         return response()->json(
-            intval($resultGroups && $resultGroupMembers)
+            Group::storeGroups($request['groupList'], $year, $month)
         );
     }
 
@@ -106,19 +92,8 @@ class GroupController extends Controller
      */
     public function destroy(int $year, int $month)
     {
-        \DB::beginTransaction();
-        try {
-            $resultGroupMember = GroupMemberRepository::deleteGroupMembersByTargetDate($year, $month);
-            $resultGroup = GroupRepository::deleteGroupsByTargetDate($year, $month);
-            \DB::commit();
-        } catch (\Exception $e) {
-            \DB::rollback();
-            return response()->json(
-                $e->getMessage()
-            );
-        }
         return response()->json(
-            intval($resultGroupMember && $resultGroup)
+            Group::destroyGroups($year, $month)
         );
     }
 }
