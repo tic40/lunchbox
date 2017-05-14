60a83f8645a8ec974b1bbb99386f671e02beb05f
tic40 <ccpzjoh@gmail.com>
2017-05-14 12:09:18 +0900

    disable register function #9

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,21 @@
 |
 */
 
-Auth::routes();
+//Auth::routes();
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+// Registration Routes...
+// $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+// $this->post('register', 'Auth\RegisterController@register');
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
 
 Route::get('/', 'IndexController@index');
 Route::get('/group', 'GroupController@index');
